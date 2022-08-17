508cd03ffc8600e32ea3853264b35f9068325db8
conrado <conrado.spereira@gmail.com>
2022-08-17 13:05:29 -0300

    Creating an interface with Feign.

---
+++ src/main/java/com/project/api/consumeapi/interfaces/CepService.java
@@ -0,0 +1,13 @@
+package com.project.api.consumeapi.interfaces;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(url = "https://viacep.com.br/ws/" , name = "viacep")
+public interface CepService {
+
+    @GetMapping("{cep}/json")
+
+    Address searchAddressByCep(@PathVariable("cep") String cep)
+    
+}
