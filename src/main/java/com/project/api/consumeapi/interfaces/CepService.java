69b23e04a0d55995937f54746bb0dc0ea65a16a9
conrado <conrado.spereira@gmail.com>
2022-08-17 13:18:36 -0300

    Creating a interface with endpoint.

--- src/main/java/com/project/api/consumeapi/interfaces/CepService.java
+++ src/main/java/com/project/api/consumeapi/interfaces/CepService.java
@@ -2,12 +2,15 @@ package com.project.api.consumeapi.interfaces;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.project.api.consumeapi.model.Address;
 
 @FeignClient(url = "https://viacep.com.br/ws/" , name = "viacep")
 public interface CepService {
 
     @GetMapping("{cep}/json")
 
-    Address searchAddressByCep(@PathVariable("cep") String cep)
+    Address searchAddressByCep(@PathVariable("cep") String cep);
     
 }
