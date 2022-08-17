a00789cc1893433ec134199bf31dbd6a62886e1d
conrado <conrado.spereira@gmail.com>
2022-08-17 13:11:21 -0300

    Creating the class that will return our object.

---
+++ src/main/java/com/project/api/consumeapi/model/Address.java
@@ -0,0 +1,18 @@
+package com.project.api.consumeapi.model;
+
+import lombok.Data;
+
+@Data
+public class Address {
+    
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String ibge;
+    private String gia;
+    private String ddd;
+    private String siafi;
+}
