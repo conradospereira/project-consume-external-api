c3d7f2bc8da620e6c7190de691c8c817ed3cfd57
conrado <conrado.spereira@gmail.com>
2022-08-17 21:06:54 -0300

    Using annotations rest on class.

--- src/main/java/com/project/api/consumeapi/rest/CepRestService.java
+++ src/main/java/com/project/api/consumeapi/rest/CepRestService.java
@@ -1,15 +1,21 @@
 package com.project.api.consumeapi.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.project.api.consumeapi.interfaces.CepService;
 import com.project.api.consumeapi.model.Address;
 
+@RestController
 public class CepRestService {
     
+    @Autowired
     private CepService cepService;
 
+    @GetMapping("/{cep}")
     public ResponseEntity<Address> getCep(@PathVariable String cep) {
 
         Address address = cepService.searchAddressByCep(cep);
